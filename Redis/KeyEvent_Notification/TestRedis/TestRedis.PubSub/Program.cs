cfedae89f4ac7597247d27f03f84275a115a8b7f
jing <870796069@qq.com>
2021-11-04 15:57:01 +0800

    新增 Redis 事件通知Demo

---
+++ Redis/KeyEvent_Notification/TestRedis/TestRedis.PubSub/Program.cs
@@ -0,0 +1,34 @@
+﻿using MK.Common;
+using StackExchange.Redis;
+using System;
+
+namespace TestRedis.PubSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MKRedis.SetConn("192.168.16.192:5014,name=expire_test");
+
+            MKRedis.GetDb(3).StringSet("order:1", "1", TimeSpan.FromSeconds(2));
+            MKRedis.GetDb(3).StringSet("order:2", "2", TimeSpan.FromSeconds(4));
+            MKRedis.GetDb(3).StringSet("order:3", "3", TimeSpan.FromSeconds(6));
+            MKRedis.GetDb(3).StringSet("order:4", "4", TimeSpan.FromSeconds(8));
+            MKRedis.GetDb(3).StringSet("order:5", "5", TimeSpan.FromSeconds(10));
+            MKRedis.GetDb(3).StringSet("order:6", "6", TimeSpan.FromSeconds(12));
+            MKRedis.GetDb(3).StringSet("order:7", "7", TimeSpan.FromSeconds(14));
+            MKRedis.GetDb(3).StringSet("order:8", "8", TimeSpan.FromSeconds(14));
+            MKRedis.GetDb(3).StringSet("order:9", "9", TimeSpan.FromSeconds(12));
+
+            var sub = MKRedis.Instance.GetSubscriber();
+            sub.Subscribe("__keyevent@3__:expired", subHandle);
+
+            Console.ReadLine();
+        }
+
+        static void subHandle(RedisChannel chn, RedisValue val)
+        {
+            Console.WriteLine($"{DateTimeOffset.Now.ToUnixTimeMilliseconds()} 频道：{chn} 收到消息：{val}");
+        }
+    }
+}
---
+++ Redis/KeyEvent_Notification/TestRedis/readme.txt
@@ -0,0 +1,3 @@
+该项目为 Redis 的 事件通知项目
+当 Redis 的 Key 触发事件之后便会发送通知
+blog参考：https://www.cnblogs.com/yeyusheng/p/8533362.html
